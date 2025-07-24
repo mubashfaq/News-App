b360a9ef624683262389b5d5e6ac797d7a9ed7e2
mubashfaq <mubashfaq10@gmail.com>
2025-07-24 15:39:42 +0500

    Previous and Next Button Done

--- src/components/NewsComponent.js
+++ src/components/NewsComponent.js
@@ -7,20 +7,48 @@ export class NewsComponent extends Component {
     super();
     this.state = {
       articles: this.articles,
-      loading: true
+      loading: true,
+      page: 1,
     }
   }
 
   async componentDidMount() {
-    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=70c0aed5089247e480e6ae8f6a71c0be";
+    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=70c0aed5089247e480e6ae8f6a71c0be&pageSize=12";
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
     this.setState({
       articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
     });
 
   }
+
+  handlePrevBtn = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=70c0aed5089247e480e6ae8f6a71c0be&page=${this.state.page - 1}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  }
+
+  handleNextBtn = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 12)) {
+
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=70c0aed5089247e480e6ae8f6a71c0be&page=${this.state.page + 1}`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  }
+
 
   render() {
     return (
@@ -43,6 +71,21 @@ export class NewsComponent extends Component {
 
           </div>
         </div>
+        {/*  Pagination */}
+        <nav className='container' aria-label="Page navigation example ">
+          <ul className="pagination justify-content-between">
+            <li className="page-item">
+              <button disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePrevBtn} >
+                <span aria-hidden="true">&laquo;</span>
+              </button>
+            </li>
+            <li className="page-item">
+              <button className="btn btn-dark" onClick={this.handleNextBtn} >
+                <span aria-hidden="true">&raquo;</span>
+              </button>
+            </li>
+          </ul>
+        </nav>
       </div>
     );
   }
